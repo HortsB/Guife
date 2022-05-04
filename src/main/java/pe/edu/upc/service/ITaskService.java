b7069925ac210811325a4c18496f63c847fe8627
Luigui Mejía Mendoza <50814418+HortsB@users.noreply.github.com>
2022-05-03 22:16:46 -0500

    Update ITaskService.java

--- src/main/java/pe/edu/upc/service/ITaskService.java
+++ src/main/java/pe/edu/upc/service/ITaskService.java
@@ -1,11 +1,12 @@
 package pe.edu.upc.service;
+
 import java.util.List;
 
 import pe.edu.upc.entidades.Task;
-public interface ITaskService {
 
-	public void insert(Task ta);
-	public List<Task> list();
-	public void delete(int idTask);
-	public void update(Task ta);
+public interface ITaskService {
+public void insert(Task t);
+public List<Task>list();
+public void delete(int idTask);
+public void update(Task t);
 }
